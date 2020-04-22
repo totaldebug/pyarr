e757281e9e0aba4339f92e56a2abff6d2205791e
marksie1988 <marksie1988@users.noreply.github.com>
2020-04-22 22:08:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as r:
 
 setup(
     name='PyArr',
-    version='0.3.0',
+    version='0.3.0dev',
     install_requires=requirements,
     packages=['PyArr',],
     license='Sonarr and Radarr API Wrapper',
