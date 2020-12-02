fb04babb7e014c81e649ea9d56bd566a65fc2bd6
Steven Marks <steven.marks@rackspace.com>
2020-12-02 16:58:46 +0000

    Added Health / command / update / quality

--- pyarr/radarr_api_v3.py
+++ pyarr/radarr_api_v3.py
@@ -7,17 +7,18 @@ from .request_api import RequestAPI
 
 class RadarrAPIv3(RequestAPI):
     def __init__(
-            self,
-            host_url: str,
-            api_key: str,
-        ):
-            """Constructor requires Host-URL and API-KEY
+        self,
+        host_url: str,
+        api_key: str,
+    ):
+        """Constructor requires Host-URL and API-KEY
 
-            Args:
-                host_url (str): Host url to radarr.
-                api_key: API key from Radarr. You can find this
-            """
-            super().__init__(host_url, api_key)
+        Args:
+            host_url (str): Host url to radarr.
+            api_key: API key from Radarr. You can find this
+        """
+        super().__init__(host_url, api_key)
+
     ## Movies
     # TODO: GET Movie
     # TODO: POST Movie
@@ -69,16 +70,63 @@ class RadarrAPIv3(RequestAPI):
     ## system
 
     ## health
+    def get_health(self):
+        """Query radarr for health information"""
+        path = "/api/v3/health"
+        res = self.request_get(path)
+        return res.json()
 
     ## command
+    def post_command(self, **kwargs):
+        """Performs any of the predetermined Radarr command routines.
+
+        Kwargs:
+            Required - name (string).
+
+            Options available:
+                - ApplicationUpdate - Trigger an update of Radarr
+                - Backup - Trigger a backup routine
+                - CheckHealth - Trigger a system health check
+                - ClearBlacklist - Triggers the removal of all blacklisted movies
+                - CleanUpRecycleBin - Trigger a recycle bin cleanup check
+                - DeleteLogFiles - Triggers the removal of all Info/Debug/Trace log files
+                - DeleteUpdateLogFiles - Triggers the removal of all Update log files
+                - DownloadedMoviesScan - Triggers the scan of downloaded movies
+                - MissingMoviesSearch - Triggers a search of all missing movies
+                - RefreshMonitoredDownloads - Triggers the scan of monitored downloads
+                - RefreshMovie - Trigger a refresh / scan of library
+                    - movieIds:int[] - Specify a list of ids (comma separated) for individual movies to refresh
+
+            See https://radarr.video/docs/api/#/Command/post-command
+        Returns:
+        json response
+
+        """
+        path = "/api/v3/command"
+
+        data = kwargs
+        res = self.request_post(path, data)
+        return res.json()
 
     ## update
+    def get_update(self):
+        """Returns a list of recent updates to Radarr
+
+        Location: System > Updates
+        """
+        path = "/api/v3/update"
+        res = self.reuqest_get(path)
+        return res.json()
 
     ## quality
+    def get_quality_profiles(self):
+        """Query Radarr for quality profiles"""
+        path = "/api/v3/qualityProfile"
+        res = self.reuqest_get(path)
+        return res.json()
 
     ## calendar
 
     ## custom filters
 
     ## remote path mapping
-
