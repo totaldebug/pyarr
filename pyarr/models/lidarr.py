bf3688af9087086d3e7055bbce9351040ca8cf79
Steven Marks <marksie1988@users.noreply.github.com>
2023-07-26 14:23:50 +0000

    fix: lidarr post command no kwargs & update docs

--- pyarr/models/lidarr.py
+++ pyarr/models/lidarr.py
@@ -1,6 +1,5 @@
 from typing import Literal
 
-#: Lidarr commands.
 LidarrCommand = Literal[
     "AlbumSearch",
     "ApplicationUpdateCheck",
@@ -10,7 +9,52 @@ LidarrCommand = Literal[
     "RefreshAlbum",
     "RefreshArtist",
 ]
+"""
+Lidarr commands.
 
+    Note:
+        The parameters are supplied as `**kwargs` within the `post_command` method.
+
+DownloadedAlbumsScan:
+    Scans downloaded albums for state
+
+    Args:
+        path (str): path to files
+
+ArtistSearch:
+    searches specified artist
+
+    Args:
+        artistId (int): ID of artist
+
+RefreshArtist:
+    Refreshes all of the artists, or specific by ID
+
+    Args:
+        artistId (int, Optional): ID of Album
+
+RefreshAlbum:
+    Refreshes all of the albums, or specific by ID
+
+    Args:
+        albumId (int, Optional): ID of Album
+
+ApplicationUpdateCheck:
+    Checks for Application updates
+
+MissingAlbumSearch:
+    Search for any missing albums
+
+AlbumSearch:
+    Search for albums
+
+RssSync:
+    Synchronise RSS Feeds
+
+Backup:
+    Backup the server data
+
+"""
 
 #: Lidarr sort keys.
 LidarrSortKey = Literal[
